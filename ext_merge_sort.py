a28efe89cd3ecf0324e913bc92e7512616e420cd
deepabhat86 <60282539+deepabhat86@users.noreply.github.com>
2020-02-06 14:55:37 -0800

    Update ext_merge_sort.py

--- ext_merge_sort.py
+++ ext_merge_sort.py
@@ -10,21 +10,26 @@ afterKwayMerge = list()
 def sortList(numbersInList):
     numbersInList.sort()
 
+#read and sort individual files add it to list of list
 def processFiles(fileName, numbersInList):
+    #To simulate large files, sleep(0.1) has been introduced to the sync program to prove that async is faster.
     time.sleep(0.1)
     readFromFile(fileName,numbersInList)
     sortList(numbersInList)
     listOfLists.append(numbersInList)
 
+#reading from the file
 def readFromFile(filename,numbersInList):
     with open(inputDirectory+os.path.sep+filename, 'r') as fin:
         for line in fin:
             numbersInList.append(int(line.strip("\n"),10))   
     fin.close()    
-     
+ 
+# k-way merge sort
 def kWayMerge(*listOfList):
     return list(merge(*listOfList))
 
+#Writing to the final sorted file
 def writeToFile(afterKwayMerge):
     filename = outputDirectory+os.path.sep+"sorted.txt"
     with open(filename,'w') as fout:
